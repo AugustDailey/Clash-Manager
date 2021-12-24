dbf4d00c464a1185f8e50c46b62019dcbde3f17a
August Dailey <daileyab@rose-hulman.edu>
2021-12-23 19:37:01 -0600

    Add button logic for add clan/player

--- ClashManager/Pages/Index.cshtml.cs
+++ ClashManager/Pages/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ClashManager.Domain.Services.ClashApiGateway;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
@@ -10,16 +11,52 @@ namespace ClashManager.Pages
 {
     public class IndexModel : PageModel
     {
+        private IClashApiGatewayService ClashApiGatewayService { get; set; }
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        [BindProperty]
+        public string ClanTag { get; set; }
+        [BindProperty]
+        public string PlayerTag { get; set; }
+
+        private string DefaultValue { get; } = "#";
+
+        public IndexModel(IClashApiGatewayService clashApiGatewayService, ILogger<IndexModel> logger)
         {
+            ClashApiGatewayService = clashApiGatewayService;
             _logger = logger;
         }
 
         public void OnGet()
         {
-
+            ClanTag = DefaultValue;
+            PlayerTag = DefaultValue;
         }
+
+        public async void OnPost()
+        {
+            if (HasUserInput(ClanTag))
+            {
+                Console.WriteLine("Adding clan");
+                var clan = await ClashApiGatewayService.GetClanAsync(ClanTag);
+                ClanTag = DefaultValue;
+            }
+
+            if (HasUserInput(PlayerTag))
+            {
+                Console.WriteLine("Adding player");
+                var player = await ClashApiGatewayService.GetPlayerAsync(PlayerTag);
+                PlayerTag = DefaultValue;
+            }
+        }
+
+        private bool HasUserInput(string field)
+        {
+            // _ = false
+            // # = false
+            // #XXXXXXX = true
+            return !string.IsNullOrWhiteSpace(field) && field.Length > 1;
+        }
+
     }
 }
